8aa4dbfeca6cf3855da3e8c89d07045b69e76443
chenchao <c18435607621@163.com>
2020-05-21 00:10:20 +0800

    第一次使用idea上传git

---
+++ git/src/cn/itchen/test/TestGit.java
@@ -0,0 +1,13 @@
+package cn.itchen.test;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+    }
+}
